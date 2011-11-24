5c20769fed1d0915ea5a2d5a8c445b5c17b8e21e
sujimichi <sujimichi@googlemail.com>
2011-11-24 01:51:25 +0000

    adding some docs to the tests

--- spec/decepticon_spec.rb
+++ spec/decepticon_spec.rb
@@ -1,9 +1,51 @@
 require 'spec_helper'
-require 'fileutils'
 
-
+#Ok, these tests are nearly as insane as me, or as stupid, I'm not sure.  In anycase they need some explaination.
+#
+#The +fetch+ method (which everything else depends on) just allows arguments of a comparible structure to be passed to the appropriate 'get', 'put', 'post', 'delete', or 'xhr' method.
+#For example 
+# fetch :html, :show,    {:id => 42} --> get :show, {:id => 42}
+# fetch :html, :destroy, {:id => 42} --> delete :show, {:id => 42}
+# fetch :ajax, :edit,    {:id => 42} --> xhr :get, :edit, {:id => 42}
+#
+#So the tests for +fetch+ just need to ensure that given certain args it calls the appropriate method with the correct args.  However....
+#'get', 'put', 'post', 'delete', and 'xhr' are methods provided by rspec-rails, they are not available here, which is fine, but the tests need to have a handel on whatever object those methods are called on (which I'm sure someone wiser would just know!).  
+#I have to confess this was not true TTD, I had to get it working till it was trying to call a method and then see what object was throwing an undefined method error.  This is what I got.
+# undefined method `get' for #<RSpec::Core::ExampleGroup::Nested_1::Nested_2::Nested_5:0x00000003716be8>
+#
+#wtf is a Nested_n?  And its not always the same n.  Skipping merryily arround my ignorance, I found that if I defined the get method on RSpec::Core::ExampleGroup it would get called.
+#However because it was called on a Nested_whatever I could not do something like this;
+# instance = Nested_1.new
+# instance.should_receive(:get).with(<stuff>) 
+# Nested_1.stub!(:new => instance)
+#
+#I could not get a handle on the instance of whatever get was called on.  While I could raise the args being sent to the method defined on RSpec::Core::ExampleGroup, as it is not an instance of RSpec::Core::ExampleGroup that has the method called on it I could not use this to get a handel on the instance. This is where my approch is perhaps a little nutts.
+#
+#I have a Probe class defined and a new instance of a probe is called in each of the methods I defined on RSpec::Core::ExampleGroup.  The probe is passed the args of the method and the name of the method (and maybe some other stuff) and thats it.  Oh wait, no its not;
+#Before each test a new instance of Probe is assigned to @probe and that instance is stubbed to be returned from Probe.new.  So when the methods on RSpec::Core::ExampleGroup invoke a new Probe it's actually @probe that they get.  Therefore assertions can be made about what is passed to the probe. ie;
+# @probe.should_receive(:data).with(:show, {:id => 42})
+# @probe.request_type.should == :get
+#
+#The Probe class is terrifyingly complex as you can see.  It really is just a class with some attr_accessors.  The only reason +data+ is a defined method is that I wanted to be able to write 
+# @probe.should_receive(:data).with(:show, {:id => 42})
+#
+#rather than
+# @probe.should_receive(:data=).with(:show, {:id => 42})
+#
+#The main method of DeceptiCon is the assert_mapping method which generates the tests.  The aim of test for this method is to assert that the rspec method 'it' is called with a message and a block.
+#Again I have used a probe inside a methods 'it' defined on RSpec::Core::ExampleGroup.  It only passes the message text to the probe.  yield is called on the block, unless a var @dont_yield is set to true.  The probe can be used for to make assertions about the message text.  As far as the block goes, we are now using rspec to test code which itself writes rspec tests.  
+#
+#The generated test needs to call to either response.should be_success or response.should_not be_success.  By again defining a response method on RSpec::Core::ExampleGroup I could get a handel on response and stub in an object that assertions can be made on.  The assertion is that it should receive :success? but how to test if it is should or should_not.  By setting what success? returns the assertions in the test being tested will throw exceptions if they don't get what they expect.  
+#So if the test is that the response should be successful then on the object returned from response, ie @resp I put;
+# @resp.should_receive(:success?).and_return(true)
+#If false was used then the test would fail, becuase the test being tested would fail.  If the expectation is for the response to not be_success, then @resp needs to return false. 
+#
+#
 class RSpec::Core::Probe
-  attr_accessor :data, :format, :block, :request_type
+  attr_accessor :format, :block, :request_type
+  def data *args
+    @data = args
+  end
 end
 
 
@@ -13,7 +55,7 @@ Struct.new("Response", :success?)
 class RSpec::Core::ExampleGroup
   def get *args
     p = RSpec::Core::Probe.new
-    p.request_type = :get
+    p.request_type = :get #This is set here so that last I can assert that the probe in question is infact the one assigned to this method.
     p.format = @request.env['HTTP_ACCEPT'] || "application/html"
     p.data(args)
   end
@@ -46,7 +88,7 @@ class RSpec::Core::ExampleGroup
   def it test_message, &blk
     p = RSpec::Core::Probe.new
     p.data(test_message)
-    unless @block_yield
+    unless @dont_yield
       yield
     end
   end
@@ -59,7 +101,10 @@ class RSpec::Core::ExampleGroup
     r = Struct::Response.new
   end
 
-
+  def assert_response args
+    p = RSpec::Core::Probe.new
+    p.data(args)   
+  end
 
 end
 
@@ -72,18 +117,95 @@ class String
   end
 end
 
+
 #Writing tests in rspec which test a method that creates rspec(rails) tests.
-#
 
 include DeceptiCon 
+
 describe DeceptiCon do 
   before(:each) do 
     @probe = RSpec::Core::Probe.new
-    @probe.stub!(:data)
     RSpec::Core::Probe.stub!(:new => @probe)
     @all_actions = [:index, :show, :new, :create, :edit, :update, :destroy]
   end
 
+  describe "fetch" do 
+    before(:each) do 
+      @request = Class.new
+      @request.stub!(:env => {})
+    end
+
+    [:html, :ajax, :xml, :json].each do |format|
+
+      describe "GET, POST, PUT and DELETE requests for #{format}" do 
+        before(:each) do 
+          @probe.should_receive(:format=).with("application/#{format.eql?(:ajax) ? :js : format}")
+        end
+
+        it 'should call get with index and args' do 
+          @probe.should_receive(:request_type=).with(:get)
+          @probe.should_receive(:data).with([:index, {:limit => 10}, nil, {:message => "yo"}])
+          fetch format, :index, {:limit => 10}, nil, {:message => "yo"}
+        end
+
+        it 'should call get with show and args' do 
+          @probe.should_receive(:request_type=).with(:get)
+          @probe.should_receive(:data).with([:show, {:id => 2}, {:user_id => 42}, {:message => "flash message"}])
+          fetch format, :show, {:id => 2}, {:user_id => 42}, {:message => "flash message"}
+        end
+
+        it 'should call get with new and args' do 
+          @probe.should_receive(:request_type=).with(:get)
+          @probe.should_receive(:data).with([:new, {:id => 2}, {:user_id => 42}, {:message => "flash message"}])
+          fetch format, :new , {:id => 2}, {:user_id => 42}, {:message => "flash message"}
+        end
+
+        it 'should call get with edit and args' do 
+          @probe.should_receive(:request_type=).with(:get)
+          @probe.should_receive(:data).with([:edit, {:id => 2}, {:user_id => 42}, {:message => "flash message"}])
+          fetch format, :edit, {:id => 2}, {:user_id => 42}, {:message => "flash message"}
+        end
+
+        it 'should call post with create and args' do 
+          @probe.should_receive(:request_type=).with(:post)
+          @probe.should_receive(:data).with([:create, {:object => {:name => "test"}, :opt => "something"}])
+          fetch format, :create, {:object => {:name => "test"}, :opt => "something"}
+        end
+
+        it 'should call put with update and args' do 
+          @probe.should_receive(:request_type=).with(:put)
+          @probe.should_receive(:data).with([:update, {:object => {:name => "test"}, :id => 12}])
+          fetch format, :update, {:object => {:name => "test"}, :id => 12}
+        end
+
+        it 'should call post with destroy and args' do 
+          @probe.should_receive(:request_type=).with(:delete)
+          @probe.should_receive(:data).with([:destroy, {:object => {:name => "test"}, :opt => "something"}])
+          fetch format, :destroy, {:object => {:name => "test"}, :opt => "something"}
+        end
+
+      end      
+
+
+    end
+
+    describe "ajax and js are interchangeable" do 
+      it 'should work for ajax' do 
+        @probe.should_receive(:request_type=).with(:get)
+        @probe.should_receive(:format=).with("application/js")
+        @probe.should_receive(:data).with([:index])
+        fetch :ajax, :index
+      end
+      it 'should work for js' do 
+        @probe.should_receive(:request_type=).with(:get)
+        @probe.should_receive(:format=).with("application/js")
+        @probe.should_receive(:data).with([:index])
+        fetch :js, :index
+      end
+
+    end
+    
+  end
 
   describe "assert_mapping" do 
     before(:each) do 
@@ -92,7 +214,7 @@ describe DeceptiCon do
       Struct::Response.stub!(:new => @resp)
     end
 
-    describe "calling 'it'" do 
+    describe "calling the method 'it' with the correct message text" do 
       before(:each) do 
         @action_mapping = {
           :index => {:html => true,  :ajax => false}, 
@@ -103,7 +225,8 @@ describe DeceptiCon do
           :update =>{:html => true,  :ajax => true},
           :destroy=>{:html => true,  :ajax => true},
         }
-        @block_yield = true #don't yield the block passed to 'it' method
+        @dont_yield = true #don't yield the block passed to 'it' method
+        @probe.stub!(:data)
       end
 
       it "should call 'it' with the correct text" do 
@@ -132,8 +255,9 @@ describe DeceptiCon do
       end
     end
 
-    describe "asserting response" do 
 
+    #
+    describe "expected response" do 
       [:index, :show, :new, :create, :edit, :update, :destroy].each do |action|
         describe "#{action}" do 
           before(:each){ 
@@ -169,6 +293,29 @@ describe DeceptiCon do
             @resp.should_receive(:success?).once.and_return(false)
             @resp.should_receive(:success?).once.and_return(false)
             @resp.should_receive(:success?).once.and_return(true)
+          end
+
+        end
+      end
+
+      [:index, :show, :new, :create, :edit, :update, :destroy].each do |action|
+        describe "assert redirect" do 
+        
+          before(:each){ 
+            @action = action
+            @skip_actions = @all_actions - [@action] 
+            should_receive(:fetch).exactly(4).times
+          }
+          after(:each){ assert_mapping }
+
+          it "should assert the response was redirected (#{action})" do 
+            @action_mapping = {@action => {:html => :redirect, :ajax => :redirect, :xml => :redirect, :json => :redirect}}
+            @probe.should_receive(:data).with("should redirect requests to #{@action}:html")
+            @probe.should_receive(:data).with("should redirect requests to #{@action}:ajax")
+            @probe.should_receive(:data).with("should redirect requests to #{@action}:xml")
+            @probe.should_receive(:data).with("should redirect requests to #{@action}:json")
+            @probe.should_receive(:data).with(:redirect).exactly(4).times
+            @resp.should_not_receive(:success?)
           end
 
         end
@@ -339,90 +486,6 @@ describe DeceptiCon do
         end 
       end     
     end
-  end
-
-  describe "fetch" do 
-    before(:each) do 
-      @request = Class.new
-      @request.stub!(:env => {})
-    end
-    describe "ajax and js are interchangeable" do 
-      it 'should work for ajax' do 
-        @probe.should_receive(:request_type=).with(:get)
-        @probe.should_receive(:format=).with("application/js")
-        @probe.should_receive(:data).with([:index])
-        fetch :ajax, :index
-      end
-      it 'should work for js' do 
-        @probe.should_receive(:request_type=).with(:get)
-        @probe.should_receive(:format=).with("application/js")
-        @probe.should_receive(:data).with([:index])
-        fetch :js, :index
-      end
-
-    end
-
-    [:html, :ajax, :xml, :json].each do |format|
-
-
-      describe "GET, POST, PUT and DELETE requests for #{format}" do 
-        before(:each) do 
-          @probe.should_receive(:format=).with("application/#{format.eql?(:ajax) ? :js : format}")
-        end
-
-        it 'should call get with index' do 
-          @probe.should_receive(:request_type=).with(:get)
-          @probe.should_receive(:data).with([:index])
-          fetch format, :index
-        end
-
-        it 'should call get with index and args' do 
-          @probe.should_receive(:request_type=).with(:get)
-          @probe.should_receive(:data).with([:index, {:limit => 10}, nil, {:message => "yo"}])
-          fetch format, :index, {:limit => 10}, nil, {:message => "yo"}
-        end
-
-        it 'should call get with show and args' do 
-          @probe.should_receive(:request_type=).with(:get)
-          @probe.should_receive(:data).with([:show, {:id => 2}, {:user_id => 42}, {:message => "flash message"}])
-          fetch format, :show, {:id => 2}, {:user_id => 42}, {:message => "flash message"}
-        end
-
-        it 'should call get with new and args' do 
-          @probe.should_receive(:request_type=).with(:get)
-          @probe.should_receive(:data).with([:new, {:id => 2}, {:user_id => 42}, {:message => "flash message"}])
-          fetch format, :new , {:id => 2}, {:user_id => 42}, {:message => "flash message"}
-        end
-
-        it 'should call get with edit and args' do 
-          @probe.should_receive(:request_type=).with(:get)
-          @probe.should_receive(:data).with([:edit, {:id => 2}, {:user_id => 42}, {:message => "flash message"}])
-          fetch format, :edit, {:id => 2}, {:user_id => 42}, {:message => "flash message"}
-        end
-
-        it 'should call post with create and args' do 
-          @probe.should_receive(:request_type=).with(:post)
-          @probe.should_receive(:data).with([:create, {:object => {:name => "test"}, :opt => "something"}])
-          fetch format, :create, {:object => {:name => "test"}, :opt => "something"}
-        end
-
-        it 'should call put with update and args' do 
-          @probe.should_receive(:request_type=).with(:put)
-          @probe.should_receive(:data).with([:update, {:object => {:name => "test"}, :id => 12}])
-          fetch format, :update, {:object => {:name => "test"}, :id => 12}
-        end
-
-        it 'should call post with destroy and args' do 
-          @probe.should_receive(:request_type=).with(:delete)
-          @probe.should_receive(:data).with([:destroy, {:object => {:name => "test"}, :opt => "something"}])
-          fetch format, :destroy, {:object => {:name => "test"}, :opt => "something"}
-        end
-
-      end      
-
-
-    end
-
   end
 
 end
